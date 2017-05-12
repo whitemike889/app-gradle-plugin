eb69838042b1583c291cf7bbe1a629796a1f924f
Andras Kerekes <akerekes@users.noreply.github.com>
2017-05-12 15:26:19 -0400

    install latest cloud sdk via curl+bash (#140)

--- kokoro/continuous_mac.sh
+++ kokoro/continuous_mac.sh
@@ -5,14 +5,9 @@ set -e
 # Display commands to stderr.
 set -x
 
-curl https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-sdk-154.0.1-darwin-x86_64.tar.gz -o google-cloud-sdk-154.0.1-darwin-x86_64.tar.gz
-tar -xzf google-cloud-sdk-154.0.1-darwin-x86_64.tar.gz
-./google-cloud-sdk/install.sh
-
-./google-cloud-sdk/bin/gcloud components update
-./google-cloud-sdk/bin/gcloud components install app-engine-java
-
-GOOGLE_CLOUD_SDK_HOME="$(pwd)"/google-cloud-sdk
+curl https://sdk.cloud.google.com | bash
+GOOGLE_CLOUD_SDK_HOME=/Users/kbuilder/google-cloud-sdk
+"$GOOGLE_CLOUD_SDK_HOME"/bin/gcloud components install app-engine-java
 
 cd github/app-gradle-plugin
 ./gradlew check
