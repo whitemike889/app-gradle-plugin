48a2e19c219a2793c1cd1368e2c311b79aea8b0b
Appu <appu@google.com>
2018-12-07 16:05:57 -0500

    Fix kokoro (#306)
    * Fix release script
    
    * Fix continuous build

--- kokoro/continuous.sh
+++ kokoro/continuous.sh
@@ -5,8 +5,12 @@ set -e
 # Display commands to stderr.
 set -x
 
-cd github/app-gradle-plugin
+# workaround for gcloud update failures (remove in future) [https://issuetracker.google.com/issues/119096137]
+sudo /opt/google-cloud-sdk/bin/gcloud components remove container-builder-local
+
 sudo /opt/google-cloud-sdk/bin/gcloud components update
 sudo /opt/google-cloud-sdk/bin/gcloud components install app-engine-java
+
+cd github/app-gradle-plugin
 ./gradlew check
 # bash <(curl -s https://codecov.io/bash)
